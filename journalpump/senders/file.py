b03197085eb269989fbdcc168fc40b34c7a4a31e
Teemu Harju <tsharju@aiven.io>
2020-06-12 12:57:49 +0300

    file: fixed file sender

--- journalpump/senders/file.py
+++ journalpump/senders/file.py
@@ -3,8 +3,8 @@ from .base import LogSender
 
 class FileSender(LogSender):
     def __init__(self, *, config, **kwargs):
-        self.mark_disconnected()
         super().__init__(config=config, max_send_interval=config.get("max_send_interval", 0.3), **kwargs)
+        self.mark_disconnected()
         self.output = open(config["file_output"], "ab")
         self.mark_connected()
 
