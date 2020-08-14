87cfea24b9bf41b75c2a4b18ee64823ebca12df1
Will Rouesnel <willrouesnel@aiven.io>
2020-08-14 15:09:10 +1000

    journalpump: Full type annotations for util.py

--- journalpump/util.py
+++ journalpump/util.py
@@ -3,17 +3,26 @@
 # This file is under the Apache License, Version 2.0.
 # See the file `LICENSE` for details.
 
+from typing import Any, AnyStr, Container, IO, Iterator, Mapping, Optional, Text, Tuple, Union
+
 import contextlib
 import datetime
 import os
 import requests
+import requests.adapters
 import tempfile
+import urllib3
 
 
 @contextlib.contextmanager
-def atomic_replace_file(file_path):
+def atomic_replace_file(file_path: AnyStr) -> Iterator[IO[Any]]:
     """Open a temporary file for writing, rename to final name when done"""
-    fd, tmp_file_path = tempfile.mkstemp(prefix=os.path.basename(file_path), dir=os.path.dirname(file_path), suffix=".tmp")
+    fd, tmp_file_path = tempfile.mkstemp(
+        prefix=os.path.basename(file_path),
+        dir=os.path.dirname(file_path),
+        suffix=b".tmp" if isinstance(file_path, bytes) else ".tmp"
+    )
+
     try:
         with os.fdopen(fd, "w") as out_file:
             yield out_file
@@ -24,28 +33,41 @@ def atomic_replace_file(file_path):
         raise
 
 
+RequestsTimeoutType = Union[float, Tuple[float, float], urllib3.Timeout]
+
+
 class TimeoutAdapter(requests.adapters.HTTPAdapter):
-    def __init__(self, *args, timeout=None, **kwargs):
-        self.timeout = timeout
+    def __init__(self, *args: Any, timeout: Optional[RequestsTimeoutType] = None, **kwargs: Any) -> None:
+        self.timeout: Optional[RequestsTimeoutType] = timeout
         super(TimeoutAdapter, self).__init__(*args, **kwargs)
 
-    def send(self, *args, **kwargs):  # pylint: disable=arguments-differ,signature-differs
-        if not kwargs.get("timeout"):
-            kwargs["timeout"] = self.timeout
-        return super(TimeoutAdapter, self).send(*args, **kwargs)
+    def send(
+        self,
+        request: requests.PreparedRequest,
+        stream: bool = False,
+        timeout: Union[None, float, Tuple[float, float], Tuple[float, None]] = None,
+        verify: Union[bool, str] = True,
+        cert: Union[None, Union[bytes, Text], Container[Union[bytes, Text]]] = None,
+        proxies: Optional[Mapping[str, str]] = None
+    ) -> requests.Response:
+
+        if timeout is None:
+            timeout = self.timeout
+
+        return super().send(request, stream, timeout, verify, cert, proxies)
 
 
-def get_requests_session(*, timeout=60):
+def get_requests_session(*, timeout: float = 60.0) -> requests.Session:
     request_session = requests.Session()
-    adapter_args = {"timeout": timeout}
-    adapter = TimeoutAdapter(**adapter_args)
+    adapter = TimeoutAdapter(timeout=timeout)
     request_session.mount("http://", adapter)
     request_session.mount("https://", adapter)
     return request_session
 
 
-def default_json_serialization(obj):  # pylint: disable=inconsistent-return-statements
+def default_json_serialization(obj: Any) -> str:
     if isinstance(obj, bytes):
         return obj.decode("utf8")
     if isinstance(obj, datetime.datetime):
         return obj.isoformat()
+    return repr(obj)
