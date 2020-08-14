4d46f06168256845ea2b2e5922c947050693b99d
Will Rouesnel <willrouesnel@aiven.io>
2020-08-14 11:13:38 +1000

    journalpump: Full type annotations to statsd.py

--- journalpump/statsd.py
+++ journalpump/statsd.py
@@ -6,35 +6,40 @@ Supports telegraf's statsd protocol extension for 'key=value' tags:
   https://github.com/influxdata/telegraf/tree/master/plugins/inputs/statsd
 
 """
+from typing import Dict, Mapping, Optional, SupportsFloat
+
 import socket
+
+TagsType = Mapping[str, str]
 
 
 class StatsClient:
-    def __init__(self, host="127.0.0.1", port=8125, tags=None):
+    @staticmethod
+    def _convert_tags(tags: Optional[TagsType] = None) -> Dict[str, str]:
+        return dict(tags.items()) if tags is not None else {}
+
+    def __init__(self, host: str = "127.0.0.1", port: int = 8125, tags: Optional[TagsType] = None) -> None:
         self._dest_addr = (host, port)
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self._tags = tags or {}
+        self._tags: Dict[str, str] = self._convert_tags(tags)
 
-    def gauge(self, metric, value, tags=None):
-        self._send(metric, b"g", value, tags)
+    def gauge(self, metric: str, value: SupportsFloat, tags: Optional[TagsType] = None) -> None:
+        self._send(metric, b"g", value, self._convert_tags(tags))
 
-    def increase(self, metric, inc_value=1, tags=None):
-        self._send(metric, b"c", inc_value, tags)
+    def increase(self, metric: str, inc_value: SupportsFloat = 1, tags: Optional[TagsType] = None) -> None:
+        self._send(metric, b"c", inc_value, self._convert_tags(tags))
 
-    def timing(self, metric, value, tags=None):
-        self._send(metric, b"ms", value, tags)
+    def timing(self, metric: str, value: SupportsFloat, tags: Optional[TagsType] = None) -> None:
+        self._send(metric, b"ms", value, self._convert_tags(tags))
 
-    def unexpected_exception(self, ex, where, tags=None):
-        all_tags = {
-            "exception": ex.__class__.__name__,
-            "where": where,
-        }
-        all_tags.update(tags or {})
+    def unexpected_exception(self, ex: BaseException, where: str, tags: Optional[TagsType] = None) -> None:
+        all_tags = self._convert_tags(tags)
+        all_tags["exception"] = ex.__class__.__name__
+        all_tags["where"] = where
         self.increase("exception", tags=all_tags)
 
-    def _send(self, metric, metric_type, value, tags):
-        if None in self._dest_addr:
-            # stats sending is disabled
+    def _send(self, metric: str, metric_type: bytes, value: SupportsFloat, tags: TagsType) -> None:
+        if self._dest_addr is None:
             return
 
         # format: "user.logins,service=payroll,region=us-west:1|c"
